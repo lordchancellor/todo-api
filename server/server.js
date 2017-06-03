883487dc939c9de98ed545b4e337ec4090eb7d75
Christopher Ford <c.d.j.ford@gmail.com>
2017-06-03 08:26:48 +0100

    Modifications for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -48,6 +49,6 @@ app.get('/todos/:id', (req, res) => {
 			.catch((e) => res.status(400).send());
 });
 
-app.listen(3000, () => console.log('Started on port 3000.....'));
+app.listen(PORT, () => console.log(`Started on port ${PORT}.....`));
 
 module.exports = { app };
