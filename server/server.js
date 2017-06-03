b61939b288601706ed1eaf200c410deea1fd1595
Christopher Ford <c.d.j.ford@gmail.com>
2017-06-03 07:54:32 +0100

    Basic server setup, todo GET and POST endpoints

---
+++ server/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+	const todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save()
+			.then((doc) => {
+				res.send(doc);
+			},
+			(e) => res.status(400).send(e));
+});
+
+app.get('/todos', (req, res) => {
+	Todo.find()
+			.then((todos) => {
+				res.send({todos});
+			},
+			(e) => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req, res) => {
+	const id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		res.status(404).send();
+	}
+
+	Todo.findById(req.params.id)
+			.then((todo) => {
+				if (todo) {
+					res.send({todo});
+				} else {
+					res.status(404).send();
+				}
+			})
+			.catch((e) => res.status(400).send());
+});
+
+app.listen(3000, () => console.log('Started on port 3000.....'));
+
+module.exports = { app };
