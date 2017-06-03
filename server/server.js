e8d2892886a286ca3ada4690fa91ad5f95879ff6
lordchancellor <c.d.j.ford@gmail.com>
2017-06-03 19:05:14 +0100

    Added GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -116,6 +117,10 @@ app.post('/users', (req, res) => {
 			.catch((e) => res.status(400).send(e));
 });
 
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
+});
 
 
 app.listen(PORT, () => console.log(`Started on port ${PORT}.....`));
